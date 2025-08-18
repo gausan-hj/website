c3e97435068830665d47ac65e0781c52f7383af0
YCleeArchitect <leesinhui37@gmail.com>
2025-08-18 21:16:24 +0800

    script.js

--- script.js
+++ script.js
@@ -71,3 +71,41 @@ window.onload = () => {
   langInput.value = '';
   renderLangList('');
 };
+
+let currentIndex = 0;
+const galleryImages = [
+  "images/photo1.jpg",
+  "images/photo2.jpg",
+  "images/photo3.jpg",
+  "images/photo4.jpg",
+  "images/photo5.jpg",
+  "images/photo6.jpg",
+  "images/photo7.jpg",
+  "images/photo8.jpg",
+  "images/photo9.jpg",
+  "images/photo10.jpg",
+  "images/photo11.jpg",
+  "images/photo12.jpg",
+  "images/photo13.jpg",
+  "images/photo14.jpg",
+  "images/photo15.jpg",
+  "images/photo16.jpg"
+];
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+
+function openLightbox(index) {
+  currentIndex = index;
+  lightbox.style.display = "flex";
+  lightboxImg.src = galleryImages[index];
+}
+
+function closeLightbox() {
+  lightbox.style.display = "none";
+}
+
+function changeSlide(step) {
+  currentIndex = (currentIndex + step + galleryImages.length) % galleryImages.length;
+  lightboxImg.src = galleryImages[currentIndex];
+}
